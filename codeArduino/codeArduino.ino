dfdd091e8a3a47c8239900f282bc0b4937172a64
Mouadh <mouadhkouki58@gmail.com>
2026-02-06 15:23:28 +0100

    ajout mqtt

--- codeArduino/codeArduino.ino
+++ codeArduino/codeArduino.ino
@@ -1,4 +1,6 @@
 #include <DHT.h>
+#include <WiFi.h> 
+#include <PubSubClient.h>
 
 // ------------------- Pin definitions -------------------
 #define DHTPIN 4
@@ -12,6 +14,15 @@
 #define HUMIDITY_THRESHOLD 60.0   // %
 #define TEMP_THRESHOLD     50.0   // Celsius
 
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
+const char* mqtt_server = "test.mosquitto.org";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+unsigned long lastLCDUpdate = 0;
+
 DHT dht(DHTPIN, DHTTYPE);
 
 // Buzzer timing
@@ -19,6 +30,56 @@ unsigned long previousMillis = 0;
 const unsigned long beepInterval = 300; // ms
 bool buzzerState = false;
 
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+
+  randomSeed(micros());
+
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  }
+
+  void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }}
+
+  void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    String clientId = "ESP32Client-";
+    clientId += String(random(0xffff), HEX);
+    if (client.connect(clientId.c_str())) {
+      Serial.println("Connected");
+      client.publish("/CapteurTempHum/Publish", "Welcome");
+      client.subscribe("/CapteurTempHum/Subscribe");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }}
+  }
 void setup() {
   Serial.begin(115200);
 
@@ -31,11 +92,18 @@ void setup() {
   digitalWrite(BUZZER, LOW);
 
   dht.begin();
-
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
   Serial.println("ESP32 + DHT22 Monitoring Started...");
 }
 
 void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();  
+
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
 
@@ -51,6 +119,11 @@ void loop() {
   Serial.print("Temperature: ");
   Serial.print(temperature);
   Serial.println(" °C");
+
+  String t = String(temperature, 2);
+  client.publish("/CapteurTempHum/temp", t.c_str()); 
+  String h = String(humidity, 1);
+  client.publish("/CapteurTempHum/hum", h.c_str()); 
 
   bool alert = (humidity > HUMIDITY_THRESHOLD || temperature > TEMP_THRESHOLD);
 
